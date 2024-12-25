7be70845dc5ce16430d60e1af3274127f70442ea
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-25 14:24:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-// models.js
+   // class person {
+
+   //          constructor(name, age) {
+   //              this.name = name;
+   //              this.age = age;
+
+   //          }
+
+   //          greet() {
+   //              console.log(`Hello,my name is ${this.name},I am ${this.age} years old.`);
+   //          }
+
+   //      }
+
+   //      new person();
+   //      let access = new person("dharmidner", "38");
+   //      access.greet();
+
+
+   //      // Create an Employee Class:
+   //      class Employee {
+   //          constructor(name, age, jobtitle) {
+   //              this.name = name;
+   //              this.age = age;
+   //              this.jobtitle = jobtitle;
+   //          }
+   //          joggreet() {
+   //              console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobtitle}.`);
+   //          }
+   //      }
+
+
+   //      let some = new Employee("dharmidner", "25", "software developer");
+   //      some.joggreet();
 class Person {
     constructor(name, age) {
         this.name = name;
@@ -21,5 +54,8 @@ class Employee extends Person {
     }
 }
 
-// Export the classes
-export { Person, Employee };
+let access = new Person("Dharminder", 38);
+access.greet();
+
+let some = new Employee("Dharminder", 25, "Software Developer");
+some.jobGreet();
