6d1ab7bfa70685d9c869ce7c71ae3fcb6e1f3ca5
dharminder <144053986+DharminderPal@users.noreply.github.com>
2024-12-25 14:05:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-   class person {
+// Person class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
 
-            constructor(name, age) {
-                this.name = name;
-                this.age = age;
+    greet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+}
 
-            }
+// Create a new instance of the Person class
+let access = new Person("Dharminder", 38);  // Corrected the arguments for age and name.
+access.greet();
 
-            greet() {
-                console.log(`Hello,my name is ${this.name},I am ${this.age} years old.`);
-            }
+// Employee class extending Person
+class Employee extends Person {
+    constructor(name, age, jobTitle) {
+        super(name, age); // Calls the constructor of Person class
+        this.jobTitle = jobTitle;
+    }
 
-        }
+    jobGreet() {
+        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+}
 
-        new person();
-        let access = new person("dharmidner", "38");
-        access.greet();
-
-
-        // Create an Employee Class:
-        class Employee {
-            constructor(name, age, jobtitle) {
-                this.name = name;
-                this.age = age;
-                this.jobtitle = jobtitle;
-            }
-            joggreet() {
-                console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobtitle}.`);
-            }
-        }
-
-
-        let some = new Employee("dharmidner", "25", "software developer");
-        some.joggreet();
+// Create an instance of Employee class
+let some = new Employee("Dharminder", 25, "Software Developer");
+some.jobGreet();
